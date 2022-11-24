c76779d91a464d9b7e3048492d49636ef372cff4
dudobeka <zalan.viktor95@gmail.com>
2022-11-24 15:38:51 +0100

    Feladat6

--- Excel/Form1.cs
+++ Excel/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Excel = Microsoft.Office.Interop.Excel;
+using Excel1 = Microsoft.Office.Interop.Excel;
 using System.Reflection;
 
 namespace Excel
@@ -17,11 +17,45 @@ namespace Excel
         List<Flat> flats;
         RealEstateEntities re = new RealEstateEntities();
 
+        Excel1.Application xlApp; // A Microsoft Excel alkalmazás
+        Excel1.Workbook xlWB; // A létrehozott munkafüzet
+        Excel1.Worksheet xlSheet; // Munkalap a munkafüzeten belül
+
 
         void LoadData()
         {
             flats = re.Flats.ToList();
         }
+
+        void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel1.Application();
+                xlWB = xlApp.Workbooks.Add();
+                xlSheet = xlWB.ActiveSheet;
+
+                CreateTable();
+
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Source + '\n' + ex.Message);
+                xlWB.Close(false);
+                xlApp.Quit();
+                xlApp = null;
+                xlWB = null;
+            }
+
+        }
+
+        private void CreateTable()
+        {
+
+        }
+
 
         public Form1()
         {
