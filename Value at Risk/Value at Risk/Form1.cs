6c9468dd443716f80cb98b9bd1cb39e2e3b3e5e1
dudobeka <zalan.viktor95@gmail.com>
2022-12-17 13:54:53 +0100

    Feladat5

--- Value at Risk/Value at Risk/Form1.cs
+++ Value at Risk/Value at Risk/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Value_at_Risk.Entities;
 
 namespace Value_at_Risk
 {
@@ -14,17 +15,27 @@ namespace Value_at_Risk
     {
         List<Tick> ticks;
         PortfolioEntities context = new PortfolioEntities();
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
 
         public Form1()
         {
             InitializeComponent();
             ticks = context.Tick.ToList();
             dataGridView1.DataSource = ticks;
+            CreatePorfolio();
+        }
+
+        private void CreatePorfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+            dataGridView2.DataSource = Portfolio;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
