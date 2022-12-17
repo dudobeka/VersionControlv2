8ba7e58f0f96191e62ad002f19d4978492e7fb92
dudobeka <zalan.viktor95@gmail.com>
2022-12-17 13:32:26 +0100

    Feladat4

--- Value at Risk/Value at Risk/Form1.cs
+++ Value at Risk/Value at Risk/Form1.cs
@@ -12,9 +12,16 @@ namespace Value_at_Risk
 {
     public partial class Form1 : Form
     {
+        List<Tick> ticks;
+        PortfolioEntities context = new PortfolioEntities();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
